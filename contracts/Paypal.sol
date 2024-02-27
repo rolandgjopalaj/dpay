2dd7352872853951ac46ace4fbcaffbb1ad4fe04
Roland Gjopalaj <rolandgjopalaj@gmail.com>
2024-02-27 12:32:34 +0100

    init

---
+++ contracts/Paypal.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Paypal{
+
+//Define the Owner of the smart contract 
+
+
+
+//Create Struct and Mappping for request, transaction & name
+
+
+
+//Add a name to wallet address
+
+
+
+//Create a Request
+
+
+
+//Pay a Request
+
+
+
+//Get all requests sent to a User
+
+
+
+
+//Get all historic transactions user has been apart of
+
+
+}
