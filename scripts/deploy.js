15d330fdcba49394a173f6836bb62c9be192624f
Roland Gjopalaj <rolandgjopalaj@gmail.com>
2024-02-27 17:24:31 +0100

    smart contract done

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const paypal = await hre.ethers.deployContract("Paypal");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await paypal.waitForDeployment();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`PayPal deployed to ${paypal.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
